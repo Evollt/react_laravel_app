a71d809a4413bf25c01ff7e2b960615a0167a6e1
Evollt <levniko343@gmail.com>
2023-01-08 01:19:42 +0300

    added authorization to web-application

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,25 +1,54 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import {useRef, useState} from "react";
+import axiosClient from "../axios-client.js";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
 
 const Login = () => {
+  const {setUser, setToken} = useStateContext()
+  const [errors, setErrors] = useState(null)
+  const emailRef = useRef()
+  const passwordRef = useRef()
+
   const onSubmit = (ev) => {
-    ev.preventDefault()
-  }
+    ev.preventDefault();
+    const payload = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value
+    }
+    axiosClient.post('/login', payload).then(({data}) => {
+      setUser(data.user)
+      setToken(data.token)
+    }).catch(err => {
+      const response = err.response
+      if(response && response.status === 422) {
+        setErrors(response.data.errors)
+        console.log(response.data.errors)
+      }
+    })
+  };
 
   return (
     <div className="login-signup-form animated fadeInDown">
       <div className="form">
         <form action="" onSubmit={onSubmit}>
-          <h1 className="title">
-            Login into your account
-          </h1>
-          <input type="email" placeholder="Email"/>
-          <input type="password" placeholder="Password"/>
+          <h1 className="title">Login into your account</h1>
+          {errors &&
+            <div className="alert">
+              {Object.keys(errors).map(key => (
+                <p key={key}>{errors[key][0]}</p>
+              ))}
+            </div>
+          }
+          <input ref={emailRef} type="email" placeholder="Email" />
+          <input ref={passwordRef} type="password" placeholder="Password" />
           <button className="btn btn-block">Login</button>
-          <p className="message">Not Registered? <Link to="/signup">Create an account</Link></p>
+          <p className="message">
+            Not Registered? <Link to="/signup">Create an account</Link>
+          </p>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
